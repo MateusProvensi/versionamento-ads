4ed4ffcb5d60339402705582246f022f4228b6cb
MateusProvensi <mateussprovensidev@outlook.com>
2022-04-19 19:55:14 -0300

    Initial Commit

---
+++ src/versionamento/application/Main.java
@@ -0,0 +1,5 @@
+package versionamento.application;
+
+public class Main {
+
+}
---
+++ src/versionamento/utils/Utils.java
@@ -0,0 +1,9 @@
+package versionamento.utils;
+
+public class Utils {
+
+	public String versao() {
+		return "1.00.00";
+	}
+	
+}
